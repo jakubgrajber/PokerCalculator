b469fb5d174763d897eedb3cab2521dde37db170
Jakub Grajber <grajber.jakub@gmail.com>
2021-12-29 14:26:07 +0100

    New class has been added - class Card with basic implementation

---
+++ Card.cpp
@@ -0,0 +1,93 @@
+//
+//  Card.cpp
+//  PokerCalculator
+//
+//  Created by Jakub Grajber on 29/12/2021.
+//
+
+#include "Card.hpp"
+
+
+Card::Card(int value, eColor color){
+    this->value = value;
+    this->color = color;
+}
+
+void Card::print() const{
+    this->printValue();
+    std::cout << " ";
+    this->printColor();
+    std::cout << " ";
+}
+
+void Card::printValue() const{
+    if (value < 11)
+        std::cout << value;
+    else{
+        switch (value)
+        {
+            case 11:
+            {
+                std::cout << "J";
+                break;
+            }
+            case 12:
+            {
+                std::cout << "Q";
+                break;
+            }
+            case 13:
+            {
+                std::cout << "K";
+                break;
+            }
+            case 14:
+            {
+                std::cout << "A";
+                break;
+            }
+        }
+    }
+}
+
+void Card::printColor() const{
+    switch (color)
+    {
+        case 0:
+        {
+            std::cout << "♠︎";
+            break;
+        }
+        case 1:
+        {
+            std::cout << "♣︎";
+            break;
+        }
+        case 2:
+        {
+            std::cout << "♦︎";
+            break;
+        }
+        case 3:
+        {
+            std::cout << "♥︎";
+            break;
+        }
+        case error: {
+            std::cout << "color error";
+            break;
+        }
+    }
+}
+
+bool Card::operator==(const Card &a) const {
+    return ((this->value == a.value) && (this->color == a.color));
+}
+
+bool Card::operator<(const Card &a) const {
+    return (this->value < a.value);
+}
+
+bool Card::operator>(const Card &a) const {
+    return (this->value > a.value);
+}
---
+++ Card.hpp
@@ -0,0 +1,38 @@
+//
+//  Card.hpp
+//  PokerCalculator
+//
+//  Created by Jakub Grajber on 29/12/2021.
+//
+
+#ifndef Card_hpp
+#define Card_hpp
+
+#include <stdio.h>
+#include <iostream>
+using std::ostream;
+
+enum eColor {spade = 0, club, diamond, heart, error};
+
+class Card{
+private:
+    int value;
+    eColor color;
+public:
+    Card(){};
+    Card(int value, eColor color);
+    
+    void print() const;
+    void printValue() const;
+    void printColor() const;
+    
+    bool operator==(const Card &a) const;
+    bool operator>(const Card &a) const;
+    bool operator<(const Card &a) const;
+    
+    friend ostream & operator<<(ostream &os, const Card &a);
+    
+    ~Card(){};
+};
+
+#endif /* Card_hpp */
